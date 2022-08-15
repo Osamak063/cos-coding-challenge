fc3d1d66c71cac2f1837d329a8214c0340809bad
FOLIO3\osamakhalid <osamakhalid@folio3.com>
2022-08-15 17:00:01 +0500

    Added CarOnSaleClient class with functions to calculate Aggregated Auction.

---
+++ challenges/backend/src/app/services/CarOnSaleClient/classes/CarOnSaleClient.ts
@@ -0,0 +1,48 @@
+import { ICarOnSaleClient } from "../interface/ICarOnSaleClient";
+import { AggregatedAuction } from "../dtos/AggregatedAuction";
+import { inject, injectable } from "inversify";
+import { DependencyIdentifier } from "../../../DependencyIdentifiers";
+import { ICarOnSaleRepo } from "../../../repositories/CarOnSaleRepo/interface/ICarOnSaleRepo";
+import { MathUtil } from "../../../util/MathUtil";
+import { AuctionItem } from "../../../repositories/CarOnSaleRepo/dtos/RunningAuctionsList";
+
+@injectable()
+export class CarOnSaleClient implements ICarOnSaleClient {
+
+    /**
+     *
+     */
+    constructor(@inject(DependencyIdentifier.CAR_ON_SALE_REPO) private carOnSaleRepo: ICarOnSaleRepo) {
+    }
+
+    public async getAggregatedAuctions(): Promise<AggregatedAuction> {
+        const listOfRunningAuctions = await this.carOnSaleRepo.getListOfRunningAuctions();
+
+        const { items } = listOfRunningAuctions || [];
+
+        const aggregatedAuction: AggregatedAuction = {
+            avgNumOfBids: this.getAvgNumOfBids(items),
+            total: items?.length || 0, // TODO safe checks
+            avgPercentageOfProgress: this.getAvgPercentageOfProgress(items)
+        }
+        return aggregatedAuction;
+    }
+
+    private getAvgNumOfBids(items: AuctionItem[]): number {
+        const numOfBids = items?.map(item => item.numBids) || [];
+        return MathUtil.Average(numOfBids);
+    }
+
+    private getAvgPercentageOfProgress(items: AuctionItem[]): number {
+        const avgPercentages = items?.
+            filter(item => item.minimumRequiredAsk != null && item.minimumRequiredAsk != 0)
+            .map(item => {
+                return 100 * (item.currentHighestBidValue /
+                    (item.minimumRequiredAsk))
+            })
+            || [];
+
+        const avgPercentageOfProgress = MathUtil.Average(avgPercentages);
+        return MathUtil.trunc(avgPercentageOfProgress, 4);
+    }
+}
