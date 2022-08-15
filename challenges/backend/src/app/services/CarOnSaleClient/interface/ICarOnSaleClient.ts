e446c4f3ed5d9bba44cfbe3ffe088888d37d7952
FOLIO3\osamakhalid <osamakhalid@folio3.com>
2022-08-15 16:58:18 +0500

    Added return type of Aggregated Auction result.

--- challenges/backend/src/app/services/CarOnSaleClient/interface/ICarOnSaleClient.ts
+++ challenges/backend/src/app/services/CarOnSaleClient/interface/ICarOnSaleClient.ts
@@ -1,8 +1,10 @@
+import { AggregatedAuction } from "../dtos/AggregatedAuction";
+
 /**
  * This service describes an interface to access auction data from the CarOnSale API.
  */
 export interface ICarOnSaleClient {
 
-    getRunningAuctions(): Promise<any /* TODO: Introduce a type */>
+    getAggregatedAuctions(): Promise<AggregatedAuction>
 
 }
