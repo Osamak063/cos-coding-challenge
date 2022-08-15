50dc3daf3102e977e3b08687bf6ecda324f80bc5
FOLIO3\osamakhalid <osamakhalid@folio3.com>
2022-08-15 18:07:54 +0500

    Validation against tslint: Empty constructor removed.

--- challenges/backend/src/app/services/Logger/classes/Logger.ts
+++ challenges/backend/src/app/services/Logger/classes/Logger.ts
@@ -1,15 +1,12 @@
-import {ILogger} from "../interface/ILogger";
-import {injectable} from "inversify";
+import { ILogger } from "../interface/ILogger";
+import { injectable } from "inversify";
 import "reflect-metadata";
 
 @injectable()
 export class Logger implements ILogger {
 
-    public constructor() {
-    }
-
-
     public log(message: string): void {
+        // tslint:disable-next-line: no-console
         console.log(`[LOG]: ${message}`);
     }
 
