9b2851abd1c5535f42677b5a83fe803fc3da9d79
FOLIO3\osamakhalid <osamakhalid@folio3.com>
2022-08-15 16:56:40 +0500

    Added Error handling in AuctionMonitorApp and displayed aggregated auctions.

--- challenges/backend/src/app/AuctionMonitorApp.ts
+++ challenges/backend/src/app/AuctionMonitorApp.ts
@@ -2,18 +2,26 @@ import { inject, injectable } from "inversify";
 import { ILogger } from "./services/Logger/interface/ILogger";
 import { DependencyIdentifier } from "./DependencyIdentifiers";
 import "reflect-metadata";
-
+import { ICarOnSaleClient } from "./services/CarOnSaleClient/interface/ICarOnSaleClient";
 @injectable()
 export class AuctionMonitorApp {
 
     public constructor(
-        @inject(DependencyIdentifier.LOGGER) private logger: ILogger) {
+        @inject(DependencyIdentifier.LOGGER) private logger: ILogger,
+        @inject(DependencyIdentifier.CAR_ON_SALE_CLIENT) private carOnSaleClient: ICarOnSaleClient) {
     }
 
     public async start(): Promise<void> {
         this.logger.log(`Auction Monitor started.`);
-
-        // TODO: Retrieve auctions and display aggregated information (see README.md)
+        try {
+            let aggregatedAuctions = await this.carOnSaleClient.getAggregatedAuctions();
+            this.logger.log(`Number of Auctions: ${aggregatedAuctions.total}`);
+            this.logger.log(`Average Number of Bids on an Auction: ${aggregatedAuctions.avgNumOfBids}`);
+            this.logger.log(`Average Percentage of the Auction Progress: ${aggregatedAuctions.avgPercentageOfProgress}`);
+        }
+        catch (ex) {
+            process.exit(-1);
+        }
     }
 
 }
