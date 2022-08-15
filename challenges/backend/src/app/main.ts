59511dae399c79426d07ff460ba7c332ad25f57b
FOLIO3\osamakhalid <osamakhalid@folio3.com>
2022-08-15 16:55:26 +0500

    Register new dependencies and config env variables.

--- challenges/backend/src/app/main.ts
+++ challenges/backend/src/app/main.ts
@@ -1,13 +1,23 @@
-import {Container} from "inversify";
-import {ILogger} from "./services/Logger/interface/ILogger";
-import {Logger} from "./services/Logger/classes/Logger";
-import {DependencyIdentifier} from "./DependencyIdentifiers";
-import {AuctionMonitorApp} from "./AuctionMonitorApp";
+import { Container } from "inversify";
+import { ILogger } from "./services/Logger/interface/ILogger";
+import { Logger } from "./services/Logger/classes/Logger";
+import { DependencyIdentifier } from "./DependencyIdentifiers";
+import { AuctionMonitorApp } from "./AuctionMonitorApp";
+import { ICarOnSaleClient } from "./services/CarOnSaleClient/interface/ICarOnSaleClient";
+import { CarOnSaleClient } from "./services/CarOnSaleClient/classes/CarOnSaleClient";
+import { ICarOnSaleRepo } from "./repositories/CarOnSaleRepo/interface/ICarOnSaleRepo";
+import dotenv from 'dotenv';
+import { axiosInstance } from '../app/interceptors/CarOnSaleAPIInterceptor'
+import { AxiosInstance } from "axios";
+import { CarOnSaleRepo } from "./repositories/CarOnSaleRepo/classes/CarOnSaleRepo";
 
+
+dotenv.config();
+axiosInstance.defaults.baseURL = process.env.CAR_ON_SALE_BASE_URL
 /*
  * Create the DI container.
  */
-const container = new Container({
+export const container = new Container({
     defaultScope: "Singleton",
 });
 
@@ -16,7 +26,12 @@ const container = new Container({
  */
 container.bind<ILogger>(DependencyIdentifier.LOGGER).to(Logger);
 
+container.bind<ICarOnSaleClient>(DependencyIdentifier.CAR_ON_SALE_CLIENT).to(CarOnSaleClient);
 
+container.bind<ICarOnSaleRepo>(DependencyIdentifier.CAR_ON_SALE_REPO).to(CarOnSaleRepo);
+
+container.bind<AxiosInstance>(DependencyIdentifier.AXIOS_INSTANCE)
+    .toConstantValue(axiosInstance);
 /*
  * Inject all dependencies in the application & retrieve application instance.
  */
