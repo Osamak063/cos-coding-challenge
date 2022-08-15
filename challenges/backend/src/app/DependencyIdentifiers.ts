639c1cae103978dd7e1d8b22b63dfa78c37ad374
FOLIO3\osamakhalid <osamakhalid@folio3.com>
2022-08-15 16:55:49 +0500

    Added new dependency identifiers.

--- challenges/backend/src/app/DependencyIdentifiers.ts
+++ challenges/backend/src/app/DependencyIdentifiers.ts
@@ -1,3 +1,6 @@
 export const DependencyIdentifier = {
-    LOGGER: "logger"
+    LOGGER: "logger",
+    CAR_ON_SALE_CLIENT: "CarOnSaleClient",
+    CAR_ON_SALE_REPO: "CarOnSaleRepo",
+    AXIOS_INSTANCE: "AxiosInstance"
 };
